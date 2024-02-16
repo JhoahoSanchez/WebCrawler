a239014b318adf4be5daded27c3fa0aa359a5f82
Jhoaho <jhoaho.sanchez@epn.edu.ec>
2024-02-15 21:51:38 -0500

    creating project with dependencies (JUnit and Jsoup)

---
+++ src/main/java/com/wc/webcrawler/WebCrawler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.wc.webcrawler;
+
+/**
+ *
+ * @author Jhoaho
+ */
+public class WebCrawler {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
